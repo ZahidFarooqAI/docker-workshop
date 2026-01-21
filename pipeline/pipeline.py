cb5f0caceb1fce7ea0b29a0f1a647f300ef57c0d
Muhammad Zahid Farooq <zahidfarooq009@gmail.com>
2026-01-21 16:25:56 +0000

    Add pipeline, uv environment, and Docker setup

---
+++ pipeline/pipeline.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+print("argumnets",sys.argv)
+month=int(sys.argv[1])
+df = pd.DataFrame({"day": [1, 2], "number_passenger": [3, 4]})
+df["month"]=month
+df.to_parquet(f"output_{month}.parquet")
+print(df.head())
+print(f"hello pipeline.py,month={month}")
+print("2+2=4") 
